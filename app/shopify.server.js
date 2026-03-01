6417717fa34ac57ee01040c4c442db320f55d43d
aayu <myfusioncommerce@gmail.com>
2026-03-02 04:41:22 +0530

    release_0031

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new PrismaSessionStorage(db),
   distribution: AppDistribution.AppStore,
   hooks: {
     afterAuth: async ({ session, admin }) => {
